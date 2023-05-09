86159cfcb1b8a1a4652f0cfe656e0bedb0a1378e
Vinu Rakav S <63595155+LordofUniverse@users.noreply.github.com>
2023-05-09 09:40:34 +0530

    Update label_studio.py

--- core/settings/label_studio.py
+++ core/settings/label_studio.py
@@ -5,6 +5,8 @@ import pathlib
 
 from core.settings.base import *
 
+ROOT_URLCONF = 'core.urls'
+
 DJANGO_DB = get_env('DJANGO_DB', DJANGO_DB_SQLITE)
 DATABASES = {'default': DATABASES_ALL[DJANGO_DB]}
 
